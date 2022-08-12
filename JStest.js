c43b7ced4c5667d421820c2c15a3665145c70377
a-hagi613 <abdullahi.hagi98@gmail.com>
2022-08-12 09:55:45 -0400

    some minor fixes including https error

--- JStest.js
+++ JStest.js
@@ -2,40 +2,47 @@
 
 const url =
 " https://api.themoviedb.org/3/discover/movie?api_key=0870ad4a8ba8822ee1a9f649978d935a&language=en-US&sort_by=popularity.desc&page=1";
-// "http://api.themoviedb.org/3/discover/movie?api_key=0870ad4a8ba8822ee1a9f649978d935a&language=en-US&sort_by=vote_average.desc&vote_count.gte=1000"
+
 function showMovie() {
     fetch(url)
+
       .then(res => {
         console.log("Response success");
-        // console.log(res);
         return res.json();
       })
       .then(data => {
-        console.log("We got the data");
-        console.log(data);
+        console.log("We got the data:" + data);
         data.results.forEach(element => {
           console.log(
             element.original_title +
-              ` https://image.tmdb.org/t/p/original/${element.poster_path}`
+              `https://image.tmdb.org/t/p/original/${element.poster_path}`
           );
+
           let cards = document.getElementById("cards");
+
           let div = document.createElement("div");
           div.classList.add("card");
+
           let img = document.createElement("img");
           img.src =
             "https://image.tmdb.org/t/p/original/" + element.poster_path;
+
           let h1 = document.createElement("h1");
           h1.innerHTML = element.title;
+
           let p = document.createElement("p");
           p.innerHTML = element.overview;
+
           let p2 = document.createElement("p");
           p2.classList.add("p2")
           p2.innerHTML =  element.release_date.slice(0,4);
           p2.style.fontSize = "12px";
+
           let p3 = document.createElement("p")
           p3.classList.add("p3")
           p3.innerHTML =  element.vote_average;
           p3.style.fontSize = "12px";
+
          if (element.vote_average >= 8 ) {
             p3.style.color = "#00B894"
           }
@@ -45,9 +52,11 @@ function showMovie() {
           else {
             p3.style.color= "#D63031"
           }
+
           let btn1 = document.createElement("button");
           btn1.classList.add("btn1");
           btn1.innerHTML = " Watch Movie";
+
           // Append newly created elements
           div.appendChild(img);
           div.appendChild(h1);
@@ -55,52 +64,56 @@ function showMovie() {
           div.appendChild(p2);
           p2.appendChild(p3);
           div.appendChild(btn1);
-          // cards.innerHTML = "";
           cards.append(div);
         });
       });
   }
 //This shows the most popular movies when the page loads
 showMovie()
-// JS Ends
+//
 
 //This code is for the trending page and shows the highest rated movies of all time
 function trendingMovie() {
     const url =
-    "http://api.themoviedb.org/3/discover/movie?api_key=0870ad4a8ba8822ee1a9f649978d935a&language=en-US&sort_by=vote_average.desc&vote_count.gte=1000"
-    // https://api.themoviedb.org/3/movie/top_rated?api_key=0870ad4a8ba8822ee1a9f649978d935a&language=en-US&page=1
+    "https://api.themoviedb.org/3/discover/movie?api_key=0870ad4a8ba8822ee1a9f649978d935a&language=en-US&sort_by=vote_average.desc&vote_count.gte=1000"
+    
         fetch(url)
           .then(res => {
             console.log("Response success");
-            // console.log(res);
             return res.json();
           })
           .then(data => {
-            console.log("We got the data");
-            console.log(data);
+            console.log("We got the data" + data);
             data.results.forEach(element => {
               console.log(
                 element.original_title +
                   ` https://image.tmdb.org/t/p/original/${element.poster_path}`
               );
               let cards = document.getElementById("cards");
+
               let div = document.createElement("div");
               div.classList.add("card");
+
               let img = document.createElement("img");
               img.src =
                 "https://image.tmdb.org/t/p/original/" + element.poster_path;
+
               let h1 = document.createElement("h1");
               h1.innerHTML = element.title;
+
               let p = document.createElement("p");
               p.innerHTML = element.overview;
+
               let p2 = document.createElement("p");
               p2.classList.add("p2")
               p2.innerHTML =  element.release_date.slice(0,4);
               p2.style.fontSize = "12px";
+
               let p3 = document.createElement("p")
               p3.classList.add("p3")
               p3.innerHTML =  element.vote_average;
               p3.style.fontSize = "12px";
+
              if (element.vote_average >= 8 ) {
                 p3.style.color = "#00B894"
               }
@@ -110,6 +123,7 @@ function trendingMovie() {
               else {
                 p3.style.color= "#D63031"
               }
+
               let btn1 = document.createElement("button");
               btn1.classList.add("btn1");
               btn1.innerHTML = " Watch Movie";
@@ -120,12 +134,11 @@ function trendingMovie() {
               div.appendChild(p2);
               p2.appendChild(p3);
               div.appendChild(btn1);
-              // cards.innerHTML = "";
               cards.append(div);
             });
           });
       }
-      //The trending function above will call once you press the trending button
+      //The trending function below will call once you press the trending button
       const trending = document.querySelector(".horizontalNavItems");
       trending.addEventListener("click", function (e) {
         console.log(e.target.id);
@@ -135,43 +148,48 @@ function trendingMovie() {
         }
     })
 
-   //This code below shows the movies i search onto the console
+   //This code below shows the movies searched onto the console
    const apiKey = "api_key=0870ad4a8ba8822ee1a9f649978d935a";
    const baseUrl = "https://api.themoviedb.org/3/search/movie?";
    function searchMovie(nameOfMovie) {
      const query = baseUrl + apiKey + `&query=${nameOfMovie}`;
      fetch(query)
        .then(res => {
-         console.log("Response success");
-         // console.log(res);
+         console.log("Response success" + res);
          return res.json();
        })
        .then(data => {
-        console.log("We got the data");
-        console.log(data);
+        console.log("We got the data" + data);
         data.results.forEach(element => {
           console.log(
             element.original_title +
               ` https://image.tmdb.org/t/p/original/${element.poster_path}`
           );
           let cards = document.getElementById("cards");
+
           let div = document.createElement("div");
           div.classList.add("card");
+
           let img = document.createElement("img");
           img.src =
             "https://image.tmdb.org/t/p/original/" + element.poster_path;
+
           let h1 = document.createElement("h1");
           h1.innerHTML = element.title;
+
           let p = document.createElement("p");
           p.innerHTML = element.overview;
+
           let p2 = document.createElement("p");
           p2.classList.add("p2")
           p2.innerHTML =  element.release_date.slice(0,4);
           p2.style.fontSize = "12px";
+
           let p3 = document.createElement("p")
           p3.classList.add("p3")
           p3.innerHTML =  element.vote_average;
           p3.style.fontSize = "12px";
+
          if (element.vote_average >= 8 ) {
             p3.style.color = "#00B894"
           }
@@ -181,6 +199,7 @@ function trendingMovie() {
           else {
             p3.style.color= "#D63031"
           }
+
           let btn1 = document.createElement("button");
           btn1.classList.add("btn1");
           btn1.innerHTML = " Watch Movie";
@@ -191,15 +210,15 @@ function trendingMovie() {
           div.appendChild(p2);
           p2.appendChild(p3);
           div.appendChild(btn1);
-          // cards.innerHTML = "";
           cards.append(div);
         });
       });
    }
+   //
    searchMovie("");
+   //
 
 //This code takes the input from the search bar and logs it onto the console
-   // const search = document.getElementById("searchBox");
    const form = document.querySelector(".form");
    form.addEventListener("submit", function (e) {
      //e.preventDefault stops the page from refreshing
@@ -211,7 +230,7 @@ function trendingMovie() {
      //the searchMovie() function adds the new searched movies
      searchMovie(form.movieName.value);
    });
-// function ends
+// 
 
 //This function fetches movies based on their genre//
 function movieGenre(id) {
@@ -223,31 +242,37 @@ function movieGenre(id) {
         return res.json();
       })
       .then(data => {
-        console.log("We got the data");
-        console.log(data);
+        console.log("We got the data" + data);
         data.results.forEach(element => {
           console.log(
             element.original_title +
               ` https://image.tmdb.org/t/p/original/${element.poster_path}`
           );
           let cards = document.getElementById("cards");
+
           let div = document.createElement("div");
           div.classList.add("card");
+
           let img = document.createElement("img");
           img.src =
             "https://image.tmdb.org/t/p/original/" + element.poster_path;
+
           let h1 = document.createElement("h1");
           h1.innerHTML = element.title;
+
           let p = document.createElement("p");
           p.innerHTML = element.overview;
+
           let p2 = document.createElement("p");
           p2.classList.add("p2")
           p2.innerHTML =  element.release_date.slice(0,4);
           p2.style.fontSize = "12px";
+
           let p3 = document.createElement("p")
           p3.classList.add("p3")
           p3.innerHTML =  element.vote_average;
           p3.style.fontSize = "12px";
+
          if (element.vote_average >= 8 ) {
             p3.style.color = "#00B894"
           }
@@ -257,9 +282,11 @@ function movieGenre(id) {
           else {
             p3.style.color= "#D63031"
           }
+
           let btn1 = document.createElement("button");
           btn1.classList.add("btn1");
           btn1.innerHTML = " Watch Movie";
+
           // Append newly created elements
           div.appendChild(img);
           div.appendChild(h1);
@@ -267,11 +294,12 @@ function movieGenre(id) {
           div.appendChild(p2);
           p2.appendChild(p3);
           div.appendChild(btn1);
-          // cards.innerHTML = "";
           cards.append(div);
         });
       });
   }
+
+  //this code below listens to the genre button clicked and fetches the corresponding movie genre
   const nav = document.querySelector(".nav");
   nav.addEventListener("click", function (e) {
     console.log(e.target.id);
@@ -351,10 +379,8 @@ function movieGenre(id) {
         console.log('Error, something went wrong!');
     }
   });
-  // movieGenre(16)
-//
-  // movieGenre(16)
-// 
+
+
 //MOVIE ID's
 // Action          28
 // Adventure       12
